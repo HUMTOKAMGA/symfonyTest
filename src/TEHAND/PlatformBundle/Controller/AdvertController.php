e52e1c3d5186e73c213ecc0223446d67c6d1892e
andrew.humtokamga <andrew.humtokamga@FRIDF-L00029.sii.fr>
2018-12-11 13:15:59 +0100

    Add multiple root on same controller

--- src/TEHAND/PlatformBundle/Controller/AdvertController.php
+++ src/TEHAND/PlatformBundle/Controller/AdvertController.php
@@ -13,5 +13,17 @@ class AdvertController extends Controller {
 //        return new Response("Notre propre Hello World !");
         return new Response($content);
     }
+    
+    public function viewAction($id)
+    {
+        return new Response("Affichage de l'annonce d'id: ".$id);
+    }
+    
+    public function viewSlugAction($slug, $year, $_format)
+    {
+        return new Response(
+                "Je suis capable d'afficher l'annonce correspondant au slug".$slug
+                ."crée en ".$year." et au format ".$_format.".");
+    }
 
 }
