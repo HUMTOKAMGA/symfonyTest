4d660dacb5f8d637f421feeea09ff847276fe013
andrew.humtokamga <andrew.humtokamga@FRIDF-L00029.sii.fr>
2018-12-11 16:04:08 +0100

    Manage Response an Request

--- src/TEHAND/PlatformBundle/Controller/AdvertController.php
+++ src/TEHAND/PlatformBundle/Controller/AdvertController.php
@@ -4,27 +4,33 @@ namespace TEHAND\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+//use Symfony\Component\HttpFoundation\Request;
 
 class AdvertController extends Controller {
 
     public function indexAction() {
         $content = $this->get('templating')
-                        ->render('TEHANDPlatformBundle:Advert:index.html.twig', 
-                                    array('id' => 5,'nom' => 'Andrew'));
+                ->render('TEHANDPlatformBundle:Advert:index.html.twig', array('id' => 5, 'nom' => 'Andrew'));
 //        return new Response("Notre propre Hello World !");
         return new Response($content);
     }
-    
-    public function viewAction($id)
-    {
-        return new Response("Affichage de l'annonce d'id: ".$id);
+
+    public function viewAction($id, Request $request) {
+        
+        $tag = $request->query->get('tag');        
+        return $this->render(
+                'TEHANDPlatformBundle:Advert:view.html.twig', array(
+                    'id' => $id, 
+                    'tag' => $tag
+                ));
     }
-    
-    public function viewSlugAction($slug, $year, $_format)
-    {
+
+    public function viewSlugAction($slug, $year, $_format) {
         return new Response(
-                "Je suis capable d'afficher l'annonce correspondant au slug".$slug
-                ."crée en ".$year." et au format ".$_format.".");
+                "Je suis capable d'afficher l'annonce correspondant au slug" . $slug
+                . "crée en " . $year . " et au format " . $_format . ".");
     }
 
 }
