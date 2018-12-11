121dc1a0375dba369691e3fd2aecb87f1981c4c9
andrew.humtokamga <andrew.humtokamga@FRIDF-L00029.sii.fr>
2018-12-11 10:25:56 +0100

    Introduct template and show

--- src/TEHAND/PlatformBundle/Controller/AdvertController.php
+++ src/TEHAND/PlatformBundle/Controller/AdvertController.php
@@ -2,12 +2,16 @@
 
 namespace TEHAND\PlatformBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class AdvertController
-{
-    public function indexAction()
-    {
-        return new Response("Notre propre Hello World !");
+class AdvertController extends Controller {
+
+    public function indexAction() {
+        $content = $this->get('templating')
+                        ->render('TEHANDPlatformBundle:Advert:index.html.twig', array('nom' => 'Andrew'));
+//        return new Response("Notre propre Hello World !");
+        return new Response($content);
     }
+
 }
