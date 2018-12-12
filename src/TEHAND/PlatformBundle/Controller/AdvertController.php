e56f05f4303cf96f590ab14556b6c2f97a663b67
andrew.humtokamga <andrew.humtokamga@FRIDF-L00029.sii.fr>
2018-12-12 10:37:19 +0100

    include add,edit,delete function

--- src/TEHAND/PlatformBundle/Controller/AdvertController.php
+++ src/TEHAND/PlatformBundle/Controller/AdvertController.php
@@ -26,20 +26,45 @@ class AdvertController extends Controller {
         ));
     }
 
+    /*
+     * function use to add annonce
+     */
     public function addAction(Request $request) {
-        /*
-         * gestion de variable flash en session
-         */
-        $session = $request->getSession();
-
-        $session->getFlashBag()->add('info', 'Annonce bien enregistrée');
-        $session->getFlashBag()->add('info', 'Oui oui cette annonce à été bien enregistrée');
-
-        return $this->redirectToRoute('tehan_platform_view', array(
-                    'id' => 8
-        ));
+        
+        if($request->isMethod('POST')){
+            $request->getSession()->getFlashBag()->add('notice','Annonce bien enregistrée');
+            
+            return $this->redirectToRoute('tehan_platform_view', array(
+                'id' => 5
+            ));
+        }
+        
+        return $this->redirectToRoute('TEHANDPlatformBundle:Advert:add.html.twig');
     }
 
+    /*
+     * @id the parrams of edit action
+     * function use to edit annonce
+     */
+    public function editAction($id, Request $request) {
+        
+        if($request->isMethod('POST')){
+            $request->getSession()->getFlashBag()->add('notice','Annonce bien modifiée');
+            
+            return $this->redirectToRoute('tehan_platform_view', array(
+                'id' => 5
+            ));
+        }
+        
+        return $this->redirectToRoute('TEHANDPlatformBundle:Advert:edit.html.twig');
+    }
+    
+    public function deleteAction($id){
+        
+        
+        return $this->render('TEHANDPlatformBundle:Advert:delete.html.twig');
+    }
+    
     public function viewSlugAction($slug, $year, $_format) {
         return new Response(
                 "Je suis capable d'afficher l'annonce correspondant au slug" . $slug
@@ -70,5 +95,20 @@ class AdvertController extends Controller {
 //       $userId =$session->get('user_id');
 //       $session->set('user_id',91);
 //       return new Response("<body>Je suis une page de test, je n'ai rien à dire</body>");
+//    }
+    
+    
+//     public function addAction(Request $request) {
+//        /*
+//         * gestion de variable flash en session
+//         */
+//        $session = $request->getSession();
+//
+//        $session->getFlashBag()->add('info', 'Annonce bien enregistrée');
+//        $session->getFlashBag()->add('info', 'Oui oui cette annonce à été bien enregistrée');
+//
+//        return $this->redirectToRoute('tehan_platform_view', array(
+//                    'id' => 8
+//        ));
 //    }
 }
