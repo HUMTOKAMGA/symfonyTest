d71abc6e1185459d68e6735b495109d721437e51
andrew.humtokamga <andrew.humtokamga@FRIDF-L00029.sii.fr>
2018-12-12 10:15:15 +0100

    Manage session flash Variable

--- src/TEHAND/PlatformBundle/Controller/AdvertController.php
+++ src/TEHAND/PlatformBundle/Controller/AdvertController.php
@@ -6,9 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 //use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+
 //use Symfony\Component\HttpFoundation\RedirectResponse;
-
-
 //use Symfony\Component\HttpFoundation\Request;
 
 class AdvertController extends Controller {
@@ -19,13 +18,26 @@ class AdvertController extends Controller {
 //        return new Response("Notre propre Hello World !");
         return new Response($content);
     }
-    
-     public function viewAction($id, Request $request) {
-       $session = $request->getSession();
-       
-       $userId =$session->get('user_id');
-       $session->set('user_id',91);
-       return new Response("<body>Je suis une page de test, je n'ai rien à dire</body>");
+
+    public function viewAction($id) {
+
+        return $this->render('TEHANDPlatformBundle:Advert:view.html.twig', array(
+                    'id' => $id
+        ));
+    }
+
+    public function addAction(Request $request) {
+        /*
+         * gestion de variable flash en session
+         */
+        $session = $request->getSession();
+
+        $session->getFlashBag()->add('info', 'Annonce bien enregistrée');
+        $session->getFlashBag()->add('info', 'Oui oui cette annonce à été bien enregistrée');
+
+        return $this->redirectToRoute('tehan_platform_view', array(
+                    'id' => 8
+        ));
     }
 
     public function viewSlugAction($slug, $year, $_format) {
@@ -33,7 +45,7 @@ class AdvertController extends Controller {
                 "Je suis capable d'afficher l'annonce correspondant au slug" . $slug
                 . "crée en " . $year . " et au format " . $_format . ".");
     }
-    
+
     //    public function viewAction($id, Request $request) {
 //        
 //        $tag = $request->query->get('tag');        
@@ -43,7 +55,6 @@ class AdvertController extends Controller {
 //                    'tag' => $tag
 //                ));
 //    }
-    
 //    public function viewAction($id) {
 //        
 ////        $response = new Response(json_encode(array('id' =>$id)));
@@ -52,5 +63,12 @@ class AdvertController extends Controller {
 //        //Pareil que ceci
 //        return new JsonResponse(array('id' =>$id));
 //    }
-
+    // Gestion des sessions
+//    public function viewAction($id, Request $request) {
+//       $session = $request->getSession();
+//       
+//       $userId =$session->get('user_id');
+//       $session->set('user_id',91);
+//       return new Response("<body>Je suis une page de test, je n'ai rien à dire</body>");
+//    }
 }
